7d6bba6b0668009f659c4e206e22c000d644ea5f
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-10-29 10:00:33 -0500

    Refactor follow card rendering to use dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import { XFollowCard } from "./XFollowCard.jsx";
 import "./App.css";
 
+const users = [
+  {
+    name: "Casa del Rey",
+    userName: "icasadelrey",
+    isFollowing: true,
+  },
+  {
+    name: "El Ministerio Roka",
+    userName: "RokaStereoOficial",
+    isFollowing: false,
+  },
+  {
+    name: "NxtWave",
+    userName: "NxtWaveBand",
+    isFollowing: true,
+  },
+];
+
 export function App() {
   return (
     <section className="follow-card-container">
-      <XFollowCard userName="icasadelrey" initialIsFollowing>
-        Casa del Rey
-      </XFollowCard>
-      <XFollowCard userName="RokaStereoOficial" initialIsFollowing={false}>
-        El Ministerio Roka
-      </XFollowCard>
-      <XFollowCard userName="NxtWaveBand" initialIsFollowing>
-        NxtWave
-      </XFollowCard>
+      {users.map(({ name, userName, isFollowing }) => (
+        <XFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </XFollowCard>
+      ))}
     </section>
   );
 }
